8b3261fad2506cb8f5c6b10a8f04376aa2ee451d
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2020-11-24 23:41:58 +0100

    fu2

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { css } from "@emotion/react"
+import { css } from "@emotion/core"
 import { graphql, Link, useStaticQuery } from "gatsby"
 
 import { rhythm } from "../utils/typography"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { css } from "@emotion/react"
+import { css } from "@emotion/core"
 import { Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
