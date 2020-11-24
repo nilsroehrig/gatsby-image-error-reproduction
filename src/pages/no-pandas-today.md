97151f0bf41ffed6ffb3b184d18202099055d3b0
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2020-11-24 23:25:01 +0100

    fu

---
+++ src/pages/no-pandas-today.md
@@ -0,0 +1,11 @@
+---
+title: "No Pandas Today"
+date: "2020-11-19"
+featuredImage: ../images/nopandas.jpg
+---
+
+No pandas today. 
+
+I'm terribly sorry.
+
+Not.
---
+++ src/templates/blog-post.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Img from "gatsby-image"
+
+export default function BlogPost({ data }) {
+  const post = data.markdownRemark
+  let featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
+  return (
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <Img fluid={featuredImgFluid} />
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
